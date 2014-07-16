19c5f7a5179739431043184cf7dde7dd1e1e500b
kevrodbe <kevin.rodriguez992@gmail.com>
2014-07-16 08:20:39 -0500

    1er avance
    1er avance del proyecto Travel to Perú.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).on("ready",kalinSaluda);
+function kalinSaluda(){
+	$('.movie').jsMovie({
+        //sequence: 'animation####.jpg',
+        //from: 1,
+        //to: 120,
+        images : ['kalin.png'],
+        folder : "assets/images/",        
+        height:711,
+        width:336,
+        grid : {height:711, width:336, columns:1, rows:12},
+        showPreLoader : false,
+        playOnLoad : true
+        //loader: {path:"assets/images/kalin.png",height:50,width:50,rows:2,columns:4},        
+    });
+
+}
