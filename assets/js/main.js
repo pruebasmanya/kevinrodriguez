31862c38abaea1dba7aff3835391630674dee99f
kevrodbe <kevin.rodriguez992@gmail.com>
2014-07-17 09:06:55 -0500

    3er avance
    La parte de la maquetación está finalizada. Solo falta agregar el
    loading y hacerle test para entregarlo al 100%.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-$(document).on("ready",kalinSaluda);
+$(document).on('ready',kalinSaluda);
+$(document).on('ready',modalMenu);
+$(document).on('ready',dropdownIdioma);
 function kalinSaluda(){
 	$('.movie').jsMovie({
         //sequence: 'animation####.jpg',
@@ -10,11 +12,32 @@ function kalinSaluda(){
         width:336,
         grid : {height:711, width:336, columns:1, rows:12},
         showPreLoader : false,
-        playOnLoad : true
-        //loader: {path:"assets/images/kalin.png",height:50,width:50,rows:2,columns:4},        
+        playOnLoad : true        
     });
-    
+
     $('#scene').parallax({
     	limitY: 0
     });
+};
+function modalMenu(){
+    $('.btn-toggle-large').click(function(e){
+		e.preventDefault();
+		$('.slicknav').fadeIn('5000s');
+	});
+
+	$('.slicknav-btn').click(function(e){
+		e.preventDefault();
+		$('.slicknav').fadeOut('5000s');
+	});
+};
+function dropdownIdioma(){
+	$('.down-arrow').addClass('flaticon-arrow486');
+    $('.dropdown-toggle').click(function(e){
+		e.preventDefault();		
+		if($('.btn-group').hasClass('open')){
+			$('.down-arrow').addClass('flaticon-arrow492');
+		}else{
+			$('.down-arrow').removeClass('flaticon-arrow486').addClass('flaticon-arrow486');
+		}
+	});
 };
